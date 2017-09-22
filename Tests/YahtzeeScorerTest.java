6af49bcba4dfc22171f48b19ddd2f985d99efbe6
jordonokelly <okellyj@carleton.edu>
2017-09-21 22:23:58 -0500

    npYahtzee added

--- Tests/YahtzeeScorerTest.java
+++ Tests/YahtzeeScorerTest.java
@@ -1,13 +1,27 @@
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by okellyj on 9/21/17.
  */
 public class YahtzeeScorerTest {
+    protected YahtzeeScorer yah;
 
     @Before
     public void setup(){
-        YahtzeeScorer Yah = new YahtzeeScorer();
+        yah = new YahtzeeScorer();
+    }
+
+    /*
+    Test to see if you can only play a Yahtzee when it is free
+     */
+    @Test
+    public void npYahtzee(){
+        assertTrue(yah.playYahtzee(1,1,1,1,1));
+        assertTrue(!yah.playYahtzee(1,1,1,1,1));
     }
 }
+
+
+
--- src/YahtzeeScorer.java
+++ src/YahtzeeScorer.java
@@ -2,4 +2,17 @@
  * Created by okellyj on 9/21/17.
  */
 public class YahtzeeScorer {
+
+    private boolean openYahtzee = true;
+
+
+    public boolean playYahtzee(int d1, int d2, int d3, int d4, int d5) {
+        if (openYahtzee){
+            openYahtzee = false;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
