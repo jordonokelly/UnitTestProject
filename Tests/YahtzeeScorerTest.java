4307f4720719cf6cc956c0a0da6385477eb50a83
jordonokelly <okellyj@carleton.edu>
2017-09-21 21:14:23 -0500

    Setup

---
+++ Tests/YahtzeeScorerTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by okellyj on 9/21/17.
+ */
+public class YahtzeeScorerTest {
+
+    @Before
+    public void setup(){
+        YahtzeeScorer Yah = new YahtzeeScorer();
+    }
+}
---
+++ src/YahtzeeScorer.java
@@ -0,0 +1,5 @@
+/**
+ * Created by okellyj on 9/21/17.
+ */
+public class YahtzeeScorer {
+}
