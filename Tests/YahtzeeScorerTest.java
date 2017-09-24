b3a3f161af39efd097a576ea454f53209004241d
jordonokelly <okellyj@carleton.edu>
2017-09-24 17:35:01 -0500

    added all score methods

--- Tests/YahtzeeScorerTest.java
+++ Tests/YahtzeeScorerTest.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -9,8 +10,13 @@ public class YahtzeeScorerTest {
     protected YahtzeeScorer yah;
 
     @Before
-    public void setup(){
+    public void setUp(){
         yah = new YahtzeeScorer();
+    }
+
+    @After
+    public void tearDown(){
+        yah = null;
     }
 
     /*
@@ -65,8 +71,71 @@ public class YahtzeeScorerTest {
     }
 
     @Test
-    public void playYahtzee() {
-        assertTrue(yah.scoreYahtzee(1,1,1,1,1));
+    public void goodHandYahtzeeTest() throws Exception {
+        yah.scoreYahtzee(1,1,1,1,1);
+        assertEquals(50, yah.getScore());
+    }
+
+    @Test(expected = YahtzeeScorer.InvalidScoringCategoryException.class)
+    public void badHandYahtzeeTest() throws Exception {
+        yah.scoreYahtzee(1,1,1,2,1);
+    }
+
+    @Test(expected = YahtzeeScorer.AlreadyScoredException.class)
+    public void alreadyScoredYahtzeeTest() throws Exception {
+        yah.scoreYahtzee(1,1,1,1,1);
+        yah.scoreYahtzee(1,1,1,1,1);
+    }
+
+    @Test
+    public void goodHandLargeStraightTest() throws Exception {
+        yah.scoreLargeStraight(1,2,3,4,5);
+        assertEquals(40, yah.getScore());
+    }
+
+    @Test(expected = YahtzeeScorer.InvalidScoringCategoryException.class)
+    public void badHandLargeStraightTest() throws Exception {
+        yah.scoreLargeStraight(1,1,1,2,1);
+    }
+
+    @Test(expected = YahtzeeScorer.AlreadyScoredException.class)
+    public void alreadyScoredLargeStraightTest() throws Exception {
+        yah.scoreLargeStraight(1,2,3,4,5);
+        yah.scoreLargeStraight(1,2,3,4,5);
+    }
+
+    @Test
+    public void goodHandFullHouse() throws Exception {
+        yah.scoreFullHouse(1,1,1,5,5);
+        assertEquals(25, yah.getScore());
+    }
+
+    @Test(expected = YahtzeeScorer.InvalidScoringCategoryException.class)
+    public void badHandFullHouse() throws Exception {
+        yah.scoreLargeStraight(1,1,1,2,1);
+    }
+
+    @Test(expected = YahtzeeScorer.AlreadyScoredException.class)
+    public void alreadyScoredFullHouse() throws Exception {
+        yah.scoreFullHouse(1,1,1,5,5);
+        yah.scoreFullHouse(1,1,1,5,5);
+    }
+
+    @Test
+    public void goodHandThreeOfAKind() throws Exception {
+        yah.scoreThreeOfAKind(1,1,1,5,5);
+        assertEquals(13, yah.getScore());
+    }
+
+    @Test(expected = YahtzeeScorer.InvalidScoringCategoryException.class)
+    public void badHandThreeOfAKind() throws Exception {
+        yah.scoreThreeOfAKind(1,5,5,2,1);
+    }
+
+    @Test(expected = YahtzeeScorer.AlreadyScoredException.class)
+    public void alreadyScoredThreeOfAKind() throws Exception {
+        yah.scoreThreeOfAKind(1,1,1,5,5);
+        yah.scoreThreeOfAKind(1,1,1,5,5);
     }
 }
 
--- src/YahtzeeScorer.java
+++ src/YahtzeeScorer.java
@@ -10,14 +10,34 @@ public class YahtzeeScorer {
     private boolean openLargeStraight;
     private boolean openFullHouse;
     private boolean openThreeOfAKind;
+    private int score;
+
+
 
     public YahtzeeScorer(){
         openYahtzee = true;
         openLargeStraight = true;
         openFullHouse = true;
         openThreeOfAKind = true;
+        score = 0;
     }
 
+    public int getScore() {
+        return this.score;
+    }
+
+
+    public class AlreadyScoredException extends Exception{
+        public AlreadyScoredException(String errorText){
+            super(errorText);
+        }
+    }
+
+    public class InvalidScoringCategoryException extends Exception{
+        public InvalidScoringCategoryException(String errorText){
+            super(errorText);
+        }
+    }
     public boolean checkPlayYahtzee(int d1, int d2, int d3, int d4, int d5) {
         if (openYahtzee){
             openYahtzee = false;
@@ -90,10 +110,71 @@ public class YahtzeeScorer {
                 (dice[2] == dice[3] && dice[2] == dice[4])));
     }
 
-    public int scoreYahtzee(int d1, int d2, int d3, int d4, int d5) {
-        if (checkPlayYahtzee(d1,d2,d3,d4,d5) && checkYahtzee(d1,d2,d3,d4,d5)){
-            return 50;
+    public void scoreYahtzee(int d1, int d2, int d3, int d4, int d5) throws Exception {
+        //check to see if a Yahtzee has already been scored
+        if (checkPlayYahtzee(d1,d2,d3,d4,d5)){
+            //check to see if the hand is a valid Yahtzee
+            if (checkYahtzee(d1,d2,d3,d4,d5)) {
+                //give 50 points
+                this.score = this.score + 50;
+            }
+            else {
+                throw new InvalidScoringCategoryException("You do not have the dice to use this category");
+            }
         }
+        else{
+            throw new AlreadyScoredException("You have already played a Yahtzee"); //TODO make this not this way because the rules are different for Yahtzees
+        }
+    }
 
+    public void scoreLargeStraight(int d1, int d2, int d3, int d4, int d5) throws Exception {
+        //check to see if a Large Straight has already been scored
+        if (checkPlayLargeStraight(d1,d2,d3,d4,d5)){
+            //check to see if the hand is a valid Large Straight
+            if (checkLargeStraight(d1,d2,d3,d4,d5)) {
+                //give 40 points
+                this.score = this.score + 40;
+            }
+            else {
+                throw new InvalidScoringCategoryException("You do not have the dice to use this category");
+            }
+        }
+        else{
+            throw new AlreadyScoredException("You have already played a Large Straight");
+        }
+    }
+
+    public void scoreFullHouse(int d1, int d2, int d3, int d4, int d5) throws Exception {
+        //check to see if a Full House has already been scored
+        if (checkPlayFullHouse(d1,d2,d3,d4,d5)){
+            //check to see if the hand is a valid Full House
+            if (checkFullHouse(d1,d2,d3,d4,d5)) {
+                //give 25 points
+                this.score = this.score + 25;
+            }
+            else {
+                throw new InvalidScoringCategoryException("You do not have the dice to use this category");
+            }
+        }
+        else{
+            throw new AlreadyScoredException("You have already played a Full House");
+        }
+    }
+
+    public void scoreThreeOfAKind(int d1, int d2, int d3, int d4, int d5) throws Exception {
+        //check to see if a three of a kind has already been scored
+        if (checkPlayThreeOfAKind(d1,d2,d3,d4,d5)){
+            //check to see if the hand is a valid three of a kind
+            if (checkThreeOfAKind(d1,d2,d3,d4,d5)) {
+                //give points
+                this.score = this.score + d1 + d2 + d3 + d4 + d5;
+            }
+            else {
+                throw new InvalidScoringCategoryException("You do not have the dice to use this category");
+            }
+        }
+        else{
+            throw new AlreadyScoredException("You have already played a three of a kind");
+        }
     }
 }
