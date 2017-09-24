11021b4a9f9cc3671de094a408dcee11f447899c
jordonokelly <okellyj@carleton.edu>
2017-09-24 16:48:34 -0500

    New tests + rename
    play fullhouse
    play three of a kind
    checkYahtzee
    checkLargeStragith
    checkFullHouse

--- Tests/YahtzeeScorerTest.java
+++ Tests/YahtzeeScorerTest.java
@@ -17,15 +17,45 @@ public class YahtzeeScorerTest {
     Test to see if you can only play a Yahtzee when it is free
      */
     @Test
-    public void npYahtzee(){
+    public void playYahtzeeTest(){
         assertTrue(yah.playYahtzee(1,1,1,1,1));
         assertTrue(!yah.playYahtzee(1,1,1,1,1));
     }
 
     @Test
-    public void npLargeStraight(){
+    public void playLargeStraightTest(){
         assertTrue(yah.playLargeStraight(1,2,3,4,5));
         assertTrue(!yah.playLargeStraight(1,2,3,4,5));
+    }
+
+    @Test
+    public void playFullHouseTest(){
+        assertTrue(yah.playFullHouse(1, 1, 1, 2, 2));
+        assertTrue(!yah.playFullHouse(1, 1, 1, 2, 2));
+    }
+
+    @Test
+    public void playThreeOfAKindTest(){
+        assertTrue(yah.playThreeOfAKind(1, 1, 1, 2, 3));
+        assertTrue(!yah.playThreeOfAKind(1, 1, 1, 2, 3));
+    }
+
+    @Test
+    public void checkYahtzeeTest(){
+        assertTrue(yah.checkYahtzee(1,1,1,1,1));
+        assertTrue(!yah.checkYahtzee(1,1,1,1,2));
+    }
+
+    @Test
+    public void checkLargeStraightTest(){
+        assertTrue(yah.checkLargeStraight(1, 2, 3, 4, 5));
+        assertTrue(!yah.checkLargeStraight(1, 2, 3, 4, 6));
+    }
+
+    @Test
+    public void checkFullHouseTest(){
+        assertTrue(yah.checkFullHouse(1,1,1,2,2));
+        assertTrue(!yah.checkFullHouse(1,1,1,2,3));
     }
 }
 
--- src/YahtzeeScorer.java
+++ src/YahtzeeScorer.java
@@ -1,14 +1,21 @@
 /**
  * Created by okellyj on 9/21/17.
  */
+
+import java.util.Arrays;
+
 public class YahtzeeScorer {
 
     private boolean openYahtzee;
     private boolean openLargeStraight;
+    private boolean openFullHouse;
+    private boolean openThreeOfAKind;
 
     public YahtzeeScorer(){
         openYahtzee = true;
         openLargeStraight = true;
+        openFullHouse = true;
+        openThreeOfAKind = true;
     }
 
     public boolean playYahtzee(int d1, int d2, int d3, int d4, int d5) {
@@ -29,5 +36,48 @@ public class YahtzeeScorer {
         else{
             return false;
         }
+    }
+
+    public boolean playFullHouse(int d1, int d2, int d3, int d4, int d5) {
+        if (openFullHouse){
+            openFullHouse = false;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean playThreeOfAKind(int d1, int d2, int d3, int d4, int d5) {
+        if (openThreeOfAKind){
+            openThreeOfAKind = false;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean checkYahtzee(int d1, int d2, int d3, int d4, int d5) {
+        return d1 == d2 && d2 == d3 && d3 == d4 && d4 == d5;
+    }
+
+    public boolean checkLargeStraight(int d1, int d2, int d3, int d4, int d5) {
+        if (d1 != d2 && d1 != d3 && d1 != d4 && d1 != d5 && d2 != d3 && d2 != d4 && d2 != d5 && d3 != d4 && d3 != d5 && d4 != d5){
+            int diceSum = d1 + d2 + d3 + d4 + d5;
+            if (diceSum == 15 || diceSum == 20){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public boolean checkFullHouse(int d1, int d2, int d3, int d4, int d5) {
+        int[] dice = {d1,d2,d3,d4,d5};
+        Arrays.sort(dice);
+        //make sure we have a three of a kind and a pair
+        return ((dice[0] == dice[1] && dice[0] == dice[2] && dice[0] != dice[3] && dice[3]==dice[4]) ||
+                (dice[0] == dice[1] && dice[0] != dice[2] &&  dice[2] == dice[3] && dice[2]==dice[4]));
     }
 }
